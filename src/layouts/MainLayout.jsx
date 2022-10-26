b94e47feaae4e6aa1dae7f9083a362d5c31a931d
eminakcil <eminakcil@outlook.com>
2022-10-26 11:10:22 +0300

    initial

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from 'react-router-dom'
+import Container from '../components/Container'
+import Button from '../components/Button'
+
+import { getPath } from '../utils'
+
+const MainLayout = () => {
+  return (
+    <Container>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-center gap-2 rounded border-solid border border-gray-400 p-6 w-full">
+          <Button
+            as={Link}
+            to={'/'}
+            className="flex-1"
+            variant="gray"
+          >
+            Home
+          </Button>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </Container>
+  )
+}
+export default MainLayout
