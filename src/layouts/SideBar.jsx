1a85916c1dc7b60ea97233a951e1b9e1f11f4dd9
eminakcil <eminakcil@outlook.com>
2022-11-04 10:36:49 +0300

    emin vs damla

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -57,24 +57,6 @@ const SideBar = () => {
                 icon={FaRegChartBar}
               >
                 Stratejik Planları Görüntüle
-<<<<<<< HEAD
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="#"
-                icon={FaPencilAlt}
-              >
-                Yeni İş Planı Oluştur
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="#"
-                icon={FaTasks}
-              >
-                İş Planlarını Listele
-              </Sidebar.Item>
-
-              <Sidebar.Item
-                href="#"
-=======
               </SidebarItem>
               <SidebarItem
                 to="#"
@@ -90,7 +72,6 @@ const SideBar = () => {
               </SidebarItem>
               <SidebarItem
                 to="#"
->>>>>>> 0ce610996f9501ea68591a571cbc993350ecc446
                 icon={FaPen}
               >
                 Anket Oluştur
