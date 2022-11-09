a7eb7a32ec9d141808d9d5195a92abc46851004c
eminakcil <eminakcil@outlook.com>
2022-11-09 16:04:28 +0300

    strategicActivity.performanceIndicator

--- src/pages/StrategicPlans/components/StrategicActivityList.jsx
+++ src/pages/StrategicPlans/components/StrategicActivityList.jsx
@@ -1,4 +1,5 @@
 import { getPath } from '@/utils'
+import Divider from '@components/Divider'
 import classNames from 'classnames'
 import { useState } from 'react'
 import { Fragment, useCallback } from 'react'
@@ -73,28 +74,35 @@ const StrategicActivityList = ({
             duration={300}
             show={strategicActivityIsActive(strategicActivity._id)}
           >
-            <div className="flex justify-center pt-5">
-              <div className="inline-flex flex-col items-end px-3">
-                <span>Yıl</span>
-                <span>Hedef</span>
-                <span>Maliyet</span>
+            <div className="pt-5 space-y-3">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">Performans Göstergesi</span>
+                <span>{strategicActivity.performanceIndicator}</span>
               </div>
-              {strategicActivity.periodGoal.map((periodGoal, index) => (
-                <Fragment key={periodGoal._id}>
-                  {index > 0 && (
-                    <div className="border-r h-[48px] self-center border-solid border-gray-500"></div>
-                  )}
-                  <div className="inline-flex flex-col items-center px-3">
-                    <span>{periodGoal.strategicPeriod.title}</span>
-                    <span>{periodGoal.goal}</span>
-                    <span>{periodGoal.price}</span>
-                  </div>
-                </Fragment>
-              ))}
-              <div className="inline-flex flex-col items-start px-3">
-                <span>Yıl</span>
-                <span>Hedef</span>
-                <span>Maliyet</span>
+              <Divider />
+              <div className="flex justify-center">
+                <div className="inline-flex flex-col items-end px-3">
+                  <span>Yıl</span>
+                  <span>Hedef</span>
+                  <span>Maliyet</span>
+                </div>
+                {strategicActivity.periodGoal.map((periodGoal, index) => (
+                  <Fragment key={periodGoal._id}>
+                    {index > 0 && (
+                      <div className="border-r h-[48px] self-center border-solid border-gray-500"></div>
+                    )}
+                    <div className="inline-flex flex-col items-center px-3">
+                      <span>{periodGoal.strategicPeriod.title}</span>
+                      <span>{periodGoal.goal}</span>
+                      <span>{periodGoal.price}</span>
+                    </div>
+                  </Fragment>
+                ))}
+                <div className="inline-flex flex-col items-start px-3">
+                  <span>Yıl</span>
+                  <span>Hedef</span>
+                  <span>Maliyet</span>
+                </div>
               </div>
             </div>
           </Hideable>
