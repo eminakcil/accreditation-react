3797d10e0f986a5f39ee83b2a831d0f21ab80c66
dmlkcncat <damla.akcin11@gmail.com>
2022-11-16 15:18:16 +0300

    plan and system create

--- src/pages/StrategicPlans/StrategicPlanCreate.jsx
+++ src/pages/StrategicPlans/StrategicPlanCreate.jsx
@@ -1,19 +1,64 @@
-import { Card, Tabs } from 'flowbite-react'
+import classNames from 'classnames'
+import { Card } from 'flowbite-react'
+import { useMemo, useRef, useState } from 'react'
 import StrategicPlanForm from './components/StrategicPlanForm'
 import StrategicSystemForm from './components/StrategicSystemForm'
 
-const StrategicPlanCreate = () => {
+const StrategicPlanCreate = ({ initalTabId = 1 }) => {
+  const planRef = useRef(null)
+
+  const handleCreateSystem = (system) => {
+    setActiveTabId(2)
+    console.log(system)
+    setTimeout(() => {
+      planRef.current.createAndSelectSystem(system)
+    }, 300)
+  }
+  const tabs = [
+    {
+      id: 1,
+      title: 'Stratejik Plan Ekle',
+      element: StrategicSystemForm,
+      props: {
+        handleCreateSystem,
+      },
+    },
+    {
+      id: 2,
+      title: 'Stratejik Amaç Ekle',
+      element: StrategicPlanForm,
+      props: {
+        ref: planRef,
+      },
+    },
+  ]
+
+  const [activeTabId, setActiveTabId] = useState(initalTabId)
+
+  const activeTab = useMemo(() => tabs.find((tab) => tab.id === activeTabId), [activeTabId, tabs])
+
   return (
     <Card>
       <Card style={{ backgroundColor: '#F9FCFF' }}>
-        <Tabs.Group>
-          <Tabs.Item title="Stratejik Plan Ekle">
-            <StrategicSystemForm />
-          </Tabs.Item>
-          <Tabs.Item title="Stratejik Amaç Ekle">
-            <StrategicPlanForm />
-          </Tabs.Item>
-        </Tabs.Group>
+        <div className="flex">
+          {tabs.map((tab) => (
+            <div
+              key={tab.id}
+              className={classNames(
+                'grid place-items-center',
+                'p-4 text-sm font-medium first:ml-0 base active cursor-pointer',
+                {
+                  'bg-gray-100 text-blue-600': tab.id === activeTabId,
+                  'text-gray-500 hover:bg-gray-50 hover:text-gray-600': tab.id !== activeTabId,
+                }
+              )}
+              onClick={() => setActiveTabId(tab.id)}
+            >
+              {tab.title}
+            </div>
+          ))}
+        </div>
+        <div>{activeTab && <activeTab.element {...activeTab.props} />}</div>
       </Card>
     </Card>
   )
--- src/validations/StrategicPlanSchema.js
+++ src/validations/StrategicPlanSchema.js
@@ -3,7 +3,7 @@ import Yup from './validation'
 export const StrategicPlanSchema = Yup.object()
   .shape({
     title: Yup.string().required(),
-    periodStartYear: Yup.number().min(1000).max(9999).required(),
+    strategicSystem: Yup.string().required(),
   })
   .typeError('Bu alan geçerli bir yıl olmalıdır.')
 
