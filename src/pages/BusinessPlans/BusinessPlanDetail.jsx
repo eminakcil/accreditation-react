e2e9e1ef2c1fbd3765bba9590899b916e39a3647
eminakcil <eminakcil@outlook.com>
2022-12-13 09:50:19 +0300

    business plan toggle visibility button removed

--- src/pages/BusinessPlans/BusinessPlanDetail.jsx
+++ src/pages/BusinessPlans/BusinessPlanDetail.jsx
@@ -1,8 +1,7 @@
 import { yearFormat } from '@/utils'
 import Divider from '@components/Divider'
 import Loading from '@components/Loading'
-// import Hideable from '@pages/StrategicPlans/components/Hideable'
-import { Button, Card } from 'flowbite-react'
+import { Card } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { BusinessPlanService } from '../../services'
@@ -12,7 +11,6 @@ const BusinessPlanDetail = () => {
   const { id } = useParams()
   const [loading, setLoading] = useState(true)
   const [items, setItems] = useState(false)
-  const [show, setShow] = useState(true)
 
   useEffect(() => {
     BusinessPlanService.getById(id)
@@ -37,143 +35,121 @@ const BusinessPlanDetail = () => {
           <Divider />
         </div>
         <div className="grid lg:grid-cols-2 gap-3">
-          <div className="">
-            <div className="max-w-xl">
-              <Card>
-                <div className="max-w-xl">
-                  <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                    İş Planı Detayları
-                  </h5>
-                </div>
-                <div className="flow-root">
-                  <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-                    <li className="py-4 sm:py-6">
-                      <div className="flex items-center space-x-4">
-                        <div className="shrink-0">
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="/images/pen.png"
-                            alt="Neil image"
-                          />
-                        </div>
-                        <div className="min-w-0 flex-1">
-                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                            Performans Göstergesi
-                          </p>
-                          <p className="py-2 truncate text-sm text-gray-500 dark:text-gray-400">
-                            {items.activity.performanceIndicator}
-                          </p>
-                        </div>
+          <div className="w-full">
+            <Card>
+              <div className="max-w-xl">
+                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                  İş Planı Detayları
+                </h5>
+              </div>
+              <div className="flow-root">
+                <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                  <li className="py-4 sm:py-6">
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="/images/pen.png"
+                          alt="Neil image"
+                        />
                       </div>
-                    </li>
-                    <hr />
-                    <li className="py-3 sm:py-6">
-                      <div className="flex items-center space-x-4">
-                        <div className="shrink-0">
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="/images/pen.png"
-                            alt="Bonnie image"
-                          />
-                        </div>
-                        <div className="min-w-0 flex-1">
-                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                            İş Planının Gerçekleşeceği Tarih
-                          </p>
-                          <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
-                            {yearFormat(items.date)}
-                          </p>
-                        </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                          Performans Göstergesi
+                        </p>
+                        <p className="py-2 truncate text-sm text-gray-500 dark:text-gray-400">
+                          {items.activity.performanceIndicator}
+                        </p>
                       </div>
-                    </li>
-                    <hr />
-                    <li className="py-3 sm:py-6">
-                      <div className="flex items-center space-x-4">
-                        <div className="shrink-0">
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="/images/pen.png"
-                            alt="Michael image"
-                          />
-                        </div>
-                        <div className="min-w-0 flex-1">
-                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                            Gerçekleşme Saati
-                          </p>
-                          <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
-                            {items.time}
-                          </p>
-                        </div>
+                    </div>
+                  </li>
+                  <hr />
+                  <li className="py-3 sm:py-6">
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="/images/pen.png"
+                          alt="Bonnie image"
+                        />
                       </div>
-                    </li>
-                    <hr />
-                    <li className="py-3 sm:py-6">
-                      <div className="flex items-center space-x-4">
-                        <div className="shrink-0">
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="/images/pen.png"
-                            alt="Lana image"
-                          />
-                        </div>
-                        <div className="min-w-0 flex-1">
-                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                            Faaliyet / İş Planı Adı
-                          </p>
-                          <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
-                            {items.title}
-                          </p>
-                        </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                          İş Planının Gerçekleşeceği Tarih
+                        </p>
+                        <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
+                          {yearFormat(items.date)}
+                        </p>
                       </div>
-                    </li>
-                    <hr />
-                    <li className="pt-3 pb-0 sm:pt-6">
-                      <div className="flex items-center space-x-4">
-                        <div className="shrink-0">
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="/images/pen.png"
-                            alt="Thomas image"
-                          />
-                        </div>
-                        <div className="min-w-0 flex-1">
-                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                            Sorumlu
-                          </p>
-                          <p className="py-2 truncate text-sm text-gray-500 dark:text-gray-400">
-                            {items?.responsible?.fullName}
-                          </p>
-                        </div>
+                    </div>
+                  </li>
+                  <hr />
+                  <li className="py-3 sm:py-6">
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="/images/pen.png"
+                          alt="Michael image"
+                        />
                       </div>
-                    </li>
-                  </ul>
-                </div>
-              </Card>
-            </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                          Gerçekleşme Saati
+                        </p>
+                        <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
+                          {items.time}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <hr />
+                  <li className="py-3 sm:py-6">
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="/images/pen.png"
+                          alt="Lana image"
+                        />
+                      </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                          Faaliyet / İş Planı Adı
+                        </p>
+                        <p className=" py-2 truncate text-sm text-gray-500 dark:text-gray-400">
+                          {items.title}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <hr />
+                  <li className="pt-3 pb-0 sm:pt-6">
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="/images/pen.png"
+                          alt="Thomas image"
+                        />
+                      </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                          Sorumlu
+                        </p>
+                        <p className="py-2 truncate text-sm text-gray-500 dark:text-gray-400">
+                          {items?.responsible?.fullName}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </Card>
           </div>
           <div>
             <div className="py-2">
-              <span
-                className="font-medium"
-                style={{ color: 'red' }}
-              >
-                Dikkat!
-              </span>{' '}
-              <span style={{ color: 'grey' }}>
-                İş planının tamamlanması için Kart eklemelisiniz!
-              </span>
-              <div className="py-4">
-                <Button
-                  onClick={() => setShow((show) => !show)}
-                  outline={true}
-                  gradientDuoTone="greenToBlue"
-                >
-                  {show ? 'Kartı Gizle' : 'Kart Ekle'}
-                </Button>
-              </div>
-              {/* <Hideable show={show}> */}
               <BusinessPlanProofForm businessPlanId={id} />
-              {/* </Hideable> */}
             </div>
           </div>
         </div>
