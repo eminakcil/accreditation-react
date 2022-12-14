6b4752ac9120ee01caad891d5d3999c186926d10
eminakcil <eminakcil@outlook.com>
2022-12-14 13:42:15 +0300

    proof preview card border

--- src/pages/BusinessPlans/components/BusinessPlanProofPreview.jsx
+++ src/pages/BusinessPlans/components/BusinessPlanProofPreview.jsx
@@ -1,12 +1,12 @@
 import constants from '@/constants'
 import Button from '@components/Button'
 import FileCard from '@components/FileCard'
-import { Label } from 'flowbite-react'
+import { Card, Label } from 'flowbite-react'
 import { BsCheck } from 'react-icons/Bs'
 
 const BusinessPlanProofPreview = ({ proof }) => {
   return (
-    <div className="max-w-xl">
+    <Card>
       <div className="flex flex-col items-center pb-10">
         <img
           className="mb-3 h-24 w-24 rounded-full shadow-lg"
@@ -14,31 +14,33 @@ const BusinessPlanProofPreview = ({ proof }) => {
         />
         <span>İş Planı Kanıt Kartı</span>
       </div>
-      <div className="block mb-2 space-y-2">
-        <Label value="Açıklama:" />
-        <p>{proof.description}</p>
-      </div>
-      <div className="space-y-3">
-        <Label value="Kanıt" />
-        {proof?.path?.map((path, index) => (
-          <FileCard
-            key={index}
-            isRemovable={false}
+      <div className="border border-solid border-gray-300 p-3 rounded-md">
+        <div className="block mb-2 space-y-2">
+          <Label value="Açıklama:" />
+          <p>{proof.description}</p>
+        </div>
+        <div className="space-y-3">
+          <Label value="Kanıt" />
+          {proof?.path?.map((path, index) => (
+            <FileCard
+              key={index}
+              isRemovable={false}
+            >
+              {path}
+            </FileCard>
+          ))}
+        </div>
+        <div className="py-2">
+          <Button
+            variant="dark-blue"
+            className="flex items-center gap-2"
           >
-            {path}
-          </FileCard>
-        ))}
+            <BsCheck size={28} />
+            <span>Tamamlandı olarak işaretle</span>
+          </Button>
+        </div>
       </div>
-      <div className="py-2">
-        <Button
-          variant="dark-blue"
-          className="flex items-center gap-2"
-        >
-          <BsCheck size={28} />
-          <span>Tamamlandı olarak işaretle</span>
-        </Button>
-      </div>
-    </div>
+    </Card>
   )
 }
 export default BusinessPlanProofPreview
